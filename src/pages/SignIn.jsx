119a8480b174be97c4480728055d4c637c07aeed
Zaidi <zaidiahsan.r@gmail.com>
2023-09-17 18:49:08 -0500

    user sign in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -11,6 +12,7 @@ const SignIn = () => {
         email: '',
         password: '',
     });
+
     //Destructured to use in JSX
     const { email, password } = formData;
 
@@ -24,6 +26,27 @@ const SignIn = () => {
         }))
     };
 
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            //initialize auth
+            const auth = getAuth();
+
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            )
+
+            if (userCredential.user) {
+                navigate('/')
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="pageContainer">
@@ -34,7 +57,7 @@ const SignIn = () => {
                 </header>
 
                 <main>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <input
                             type="email"
                             className="emailInput"
