406e47cb6f0d81a9720c43426df56b279afa812b
Zaidi <zaidiahsan.r@gmail.com>
2023-09-17 21:28:39 -0500

    react toastify login/signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from '../firebase.config';
@@ -59,7 +60,7 @@ const SignUp = () => {
             //redirect to home
             navigate('/')
         } catch (error) {
-            console.log(error)
+            toast.error('Something went wrong with registration')
         }
     }
 
