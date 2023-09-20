2679efd1249971f642598b8f7e6c24e630de7a8d
Zaidi <zaidiahsan.r@gmail.com>
2023-09-20 18:29:58 -0500

    firebase rule error handling changed rule from 2mb to 5mb

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { db } from '../firebase.config';
 import { useNavigate } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
 
@@ -101,34 +104,87 @@ const CreateListing = () => {
             location = address
         }
 
+        // Store image in firebase
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+            const storage = getStorage()
+            const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+    
+            const storageRef = ref(storage, 'images/' + fileName)
+    
+            const uploadTask = uploadBytesResumable(storageRef, image)
+    
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                console.log('Upload is ' + progress + '% done')
+                switch (snapshot.state) {
+                    case 'paused':
+                    console.log('Upload is paused')
+                    break
+                    case 'running':
+                    console.log('Upload is running')
+                    break
+                    default:
+                    break
+                }
+                },
+                (error) => {
+                reject(error)
+                },
+                () => {
+                // Handle successful uploads on complete
+                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL)
+                })
+                }
+            )
+            })
+        }
+
+        //call the storeImage function for every image that is uploaded up to 6
+        const imgUrls = await Promise.all(
+            [...images].map((image) => storeImage(image))
+        ).catch(() => {
+            setLoading(false)
+            toast.error('Images not uploaded')
+            return
+        })
+
+        console.log(imgUrls)
+
         setLoading(false)
     }
 
     const onMutate = (e) => {
         let boolean = null
-
-        if(e.target.value === 'true') {
-            boolean = true
+    
+        if (e.target.value === 'true') {
+          boolean = true
         }
-        if(e.target.value === 'false') {
-            boolean = false
+        if (e.target.value === 'false') {
+          boolean = false
         }
-
-        //Files
+    
+        // Files
         if (e.target.files) {
-            setFormData((prevState) => ({
-                ...prevState,
-                images: e.target.files
-            }))
+          setFormData((prevState) => ({
+            ...prevState,
+            images: e.target.files,
+          }))
         }
-
+    
+        // Text/Booleans/Numbers
         if (!e.target.files) {
-            setFormData((prevState) => ({
-                ...prevState,
-                [e.target.id]: boolean ?? e.target.value,
-            }))
+          setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: boolean ?? e.target.value,
+          }))
         }
-    }
+      }
 
     if (loading) {
         return <Spinner />
