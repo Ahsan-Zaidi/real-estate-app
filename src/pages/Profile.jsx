48b7d90780d013a69d3e142dbf337f7c3bd6b230
Zaidi <zaidiahsan.r@gmail.com>
2023-09-21 12:47:29 -0500

    delete listings from user profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
-import { updateDoc, doc } from 'firebase/firestore';
+import { 
+    updateDoc,
+    doc,
+    collection,
+    getDocs,
+    query,
+    where,
+    orderBy,
+    deleteDoc,
+} from 'firebase/firestore';
 import { db } from '../firebase.config';
 import { useNavigate, Link } from "react-router-dom";
 import { toast } from "react-toastify";
+import ListingItem from '../components/ListingItem';
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg';
 import homeIcon from '../assets/svg/homeIcon.svg';
 
 const Profile = () => {
     const auth = getAuth();
+    const [loading, setLoading] = useState(true)
+    const [listings, setListings] = useState(null)
 
     //state to update the user details
     const [changeDetails, setChangeDetails] = useState(false);
@@ -24,6 +36,33 @@ const Profile = () => {
 
     //initialize navigate
     const navigate = useNavigate();
+
+    //Fetch the listings that match userRef field w logged in User
+    useEffect(() => {
+        const fetchUserListings = async () => {
+            const listingsRef = collection(db, 'listings')
+
+            //Only grab listings where the userRef value matches the logged in users uid
+            const q = query(listingsRef, where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'))
+
+            const querySnap = await getDocs(q)
+
+            //initialize listings array
+            let listings = []
+
+            querySnap.forEach((doc) => {
+                return listings.push({
+                    id: doc.id,
+                    data: doc.data()
+                })
+            })
+
+            setListings(listings)
+            setLoading(false)
+        }
+
+        fetchUserListings()
+    }, [auth.currentUser.uid])
 
     //logout handler
     const onLogout = () => {
@@ -58,6 +97,17 @@ const Profile = () => {
             [e.target.id]: e.target.value,
         }))
     };
+
+    const onDelete = async(listingId) => {
+        if (window.confirm('Are you sure you want to delete?')) {
+            await deleteDoc(doc(db, 'listings', listingId))
+            
+            //Once the doc is deleted I want to show the remaining listings on the page
+            const updatedListings = listings.filter((listing) => listing.id !== listingId)
+            setListings(updatedListings)
+            toast.success('You have succesfully deleted listing')
+        }
+    }
 
     return (
         <div className="profile">
@@ -112,6 +162,22 @@ const Profile = () => {
                     <p>Sell or rent your home</p>
                     <img src={arrowRight} alt="arrow right" />
                 </Link>
+
+                {!loading && listings?.length > 0 && (
+                    <>
+                        <p className="listingText">Your listings</p>
+                        <ul className="listingsList">
+                            {listings.map((listing) => (
+                                <ListingItem
+                                    key={listing.id}
+                                    listing={listing.data}
+                                    id={listing.id}
+                                    onDelete={() => onDelete(listing.id)}
+                                />
+                            ))}
+                        </ul>
+                    </>
+                )}
             </main>
         </div>
     )
